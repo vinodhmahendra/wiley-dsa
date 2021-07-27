dc3f50160c28e6b569c138dcc8b59d5e3f169eea
Vinodh Kumar <vinodh.mahendra@gmail.com>
2021-07-27 17:16:06 +0530

    intial commit

--- binary-heap/src/Program.java
+++ binary-heap/src/Program.java
@@ -4,8 +4,22 @@ public class Program {
 	public static void main(String[] args) {
 		
 		BinaryHeap newBH = new BinaryHeap(5);
-		newBH.peek();
-
+//		newBH.insert(10, "Min");
+//		newBH.insert(5, "Min");
+//		newBH.insert(15, "Min");
+//		newBH.insert(1, "Min");
+//		newBH.levelOrder();
+//		newBH.extractHeadOfBH("Min");
+//		newBH.levelOrder();
+		
+		
+		newBH.insert(10, "Max");
+		newBH.insert(5, "Max");
+		newBH.insert(15, "Max");
+		newBH.insert(1, "Max");
+		newBH.levelOrder();
+		newBH.deleteBH();
+		newBH.levelOrder();
 	}
 
 }
